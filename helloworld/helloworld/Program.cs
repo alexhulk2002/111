d4e339ff0cd8e453df19a3b2c4c246e4646eb64f
alexhulk2002 <75840346+alexhulk2002@users.noreply.github.com>
2020-12-11 14:00:44 +0200

    first upload

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("boss");
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + b);
+            double c = 1.5;
+
+            Console.WriteLine(Convert.ToString(c));
+            string x = Console.ReadLine();
+            Console.WriteLine(x);
+            string result=(c>2) ? "yes" : "no";
+            Console.WriteLine(result);
+            string[] name = { "in", "vff", "rfvr" };
+            foreach(string i in name)
+            { Console.WriteLine(i); }
+        }
+    }
+}
